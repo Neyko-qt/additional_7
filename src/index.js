884a72eac9e071d574e1610572bca8b0c98143ac
Neyko-qt <fauster08@mail.ru>
2018-03-05 21:23:31 +0300

    changed compare function

--- src/index.js
+++ src/index.js
@@ -1,63 +1,61 @@
 module.exports = function solveSudoku(matrix) {
     
+    
     for (var i = 0; i < 9; i++) {
-        for (var j = 0; j < 9; j++) {
+      for (var j = 0; j < 9; j++) {
           
-            if (matrix[i][j] === 0) {
+          if (matrix[i][j] === 0) {
             
                 for (var tmp = 1; tmp <= 9; tmp++) {
               
                     if (compareResult(matrix, i, j, tmp)) {
                         matrix[i][j] = tmp;
-                        var solution = solveSudoku(matrix);
-                
-                        if (solution) {
-                        return matrix;
-                        }
-                              
-                        matrix[i][j] = 0;
+                        if (solveSudoku(matrix) != 0) {
+                            return matrix;
+                        } else {
+                            matrix[i][j] = 0;
+                        }     
                     }
                 }
                 return false;
             }
         }
     }
-
     return true;
       
     
-    function compareResult(matrix, tmpCol, tmpRow, result) {
-      
-      var mainRow = Math.floor(i / 3) * 3;
-      var mainCol = Math.floor(j / 3) * 3;
-      
-      
+    function compareResult(matrix, tmpRow, tmpCol, tmpValue) {
+
       // check for cols
       
       for (var col = 0; col < 9; col++) {
-        if (col != j && matrix[i][col] === result) {
+        if (matrix[tmpRow][col] === tmpValue && col != tmpCol) {
           return false;
         }
       }
       
       // check for rows
       
+
       for (var row = 0; row < 9; row++) {
-        if (row != i) {
-            if (matrix[row][j] === result) {
-                return false;
-            }
+        if (matrix[row][tmpCol] === tmpValue && row != tmpRow) {
+            return false;
         }
       }
 
       // check for square 3x3 
 
+      var mainRow = Math.floor(tmpRow / 3) * 3;
+      var mainCol = Math.floor(tmpCol / 3) * 3;
+
       for (var row = 0; row < 3; row++) {
         for (var col = 0; col < 3; col++) {
-          if (row != i && col != j) {
-            if (matrix[mainRow + row][mainCol + col] === result) {
-                return false;
-            }
+          if (row != tmpRow) {
+              if (col != tmpCol) {
+                if (matrix[mainRow + row][mainCol + col] === tmpValue) {
+                    return false;
+                }
+              }
           }
         }
       }
